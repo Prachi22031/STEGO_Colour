0312c3a76398e8f4907dac65da146a069fb1e4b2
Prachi22031 <142161253+Prachi22031@users.noreply.github.com>
2024-08-06 17:03:53 +0530

    Update train_segmentation.py

--- src/train_segmentation.py
+++ src/train_segmentation.py
@@ -199,7 +199,7 @@ class LitUnsupervisedSegmenter(pl.LightningModule):
             return kmeans.labels_
 
         # Performing color-based clustering on the image
-        patch_size = 70
+        patch_size = 170
         n_clusters = self.n_classes  # Or any other number of clusters
         img_rgb = img.permute(0, 2, 3, 1).cpu().numpy()  # Convert to HWC format
         color_clusters = color_based_clustering(img_rgb[0], patch_size, n_clusters)  # Only for first image in batch
@@ -310,7 +310,7 @@ class LitUnsupervisedSegmenter(pl.LightningModule):
                 kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(avg_rgb)
                 return kmeans.labels_
 
-            patch_size = 70
+            patch_size = 170
             n_clusters = self.n_classes  
             img_rgb = img.permute(0, 2, 3, 1).cpu().numpy() 
             color_clusters = color_based_clustering(img_rgb[0], patch_size, n_clusters) 
